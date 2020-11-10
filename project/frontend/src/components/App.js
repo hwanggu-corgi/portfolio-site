411c475bab2c094ea4cef2a212a3adf10ec4863f
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-10 09:50:04 +0900

    feat: clean out files

--- project/frontend/src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import DataProvider from "./DataProvider";
-import Table from "./Table";
-const App = () => (
-  <div>hello</div>
-);
-const wrapper = document.getElementById("app");
-wrapper ? ReactDOM.render(<App />, wrapper) : null;

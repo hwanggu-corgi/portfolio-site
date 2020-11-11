a0ad4be6055d35be10dd0832ce155e5fceca3308
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-12 01:45:40 +0900

    feat: Add progress #77

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,106 +11,13 @@ import { AppProvider } from './components/Context';
 // import PrimaryHeader from './components/PrimaryHeader';
 // import PrimaryHeaderMobile from './components/PrimaryHeaderMobile';
 
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/Home/HomeScreen';
 import './App.scss';
 
 class App extends Component {
 
     state = {
-        user: {
-            name: null,
-            email: null,
-            authToken: null
-        },
-        recentItems: [
-            {
-                pk: 1,
-                name: 'Page 1',
-                path: 'physics/lecture1/page1',
-                imageURL: 'https://via.placeholder.com/200'
-            },
-            {
-                pk: 2,
-                name: 'Page 2',
-                path: 'physics/lecture1/page2',
-                imageURL: 'https://via.placeholder.com/200'
-            },
-            {
-                pk: 3,
-                name: 'Page 3',
-                path: 'physics/lecture1/page3',
-                imageURL: 'https://via.placeholder.com/200'
-            }
-        ],
-        notes: {
-            slug: null,
-            imported: false,
-            items: []
-        },
-        folders: {
-            info: {
-                slug: 'physics',
-                name: 'Physics',
-                path: 'physics/'
-            },
-            items: [
-                {
-                    pk: 1,
-                    path: 'physics/lecture1',
-                    name: 'Lecture 1'
-                },
-                {
-                    pk: 2,
-                    path: 'physics/lecture2',
-                    name: 'Lecture 2'
-                },
-                {
-                    pk: 3,
-                    path: 'physics/lecture3',
-                    name: 'Lecture 3'
-                }
-            ]
-        },
-        pages: {
-            info: {
-                slug: 'lecture1',
-                name: 'Lecture 1',
-                path: 'physics/lecture1/'
-            },
-            currentIndex: null,
-            items: [
-                {
-                    pk: 1,
-                    name: 'Page 1',
-                    path: 'physics/lecture1/page1',
-                    imageURL: 'https://via.placeholder.com/200'
-                },
-                {
-                    pk: 2,
-                    name: 'Page 2',
-                    path: 'physics/lecture1/page2',
-                    imageURL: 'https://via.placeholder.com/200'
-                },
-                {
-                    pk: 3,
-                    name: 'Page 3',
-                    path: 'physics/lecture1/page3',
-                    imageURL: 'https://via.placeholder.com/200'
-                },
-                {
-                    pk: 4,
-                    name: 'Page 4',
-                    path: 'physics/lecture1/page4',
-                    imageURL: 'https://via.placeholder.com/200'
-                },
-                {
-                    pk: 5,
-                    name: 'Page 5',
-                    path: 'physics/lecture1/page5',
-                    imageURL: 'https://via.placeholder.com/200'
-                }
-            ]
-        }
+
     }
 
     render() {
@@ -118,13 +25,14 @@ class App extends Component {
             <AppProvider value={{...this.state}}>
                 <BrowserRouter basename={"portfolio-site"}>
                     <div className="App">
+
                         {/* <PrimaryHeaderMobile/>
                         <PrimaryHeader/> */}
                         <Switch>
-                            <Route path="/home" component={HomeScreen}/>
-                            {/* <Route path="/about" component={LogOutScreen}/>
-                            <Route path="/works" component={LogOutScreen}/>
-                            <Route component={NotFoundScreen}/> */}
+                            {/* <Route path="/about" component={AboutScreen}/>
+                            <Route path="/works" component={WorksScreen}/> */}
+                            <Route path="/" component={HomeScreen}/>
+                            {/* <Route component={NotFoundScreen}/> */}
                         </Switch>
                     </div>
                 </BrowserRouter>

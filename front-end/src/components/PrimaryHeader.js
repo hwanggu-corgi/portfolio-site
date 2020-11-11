52a94c51526f58103f81f9eacd3c37fbeb4a5617
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-12 04:00:51 +0900

    feat: Add progress #84

--- front-end/src/components/PrimaryHeader.js
+++ front-end/src/components/PrimaryHeader.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
 import { NavLink, withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Logo from '../img/moe.jpeg';
 
@@ -11,7 +11,7 @@ class PrimaryHeader extends Component {
             <header className="header--primary-desktop">
                 <img className="logo" src={Logo} alt="Hyungmo Gu"/>
                 <nav className="menu menu--primary mb-4">
-                    <h3 className="mb-9"><a href="#">Hyungmo Gu</a></h3>
+                    <h3 className="mb-9"><NavLink to="/">Hyungmo Gu</NavLink></h3>
                     <ul>
                         <li className="mb-2"><NavLink to="/">Home</NavLink></li>
                         <li className="mb-2"><NavLink to="/about">About</NavLink></li>
@@ -21,8 +21,8 @@ class PrimaryHeader extends Component {
                 </nav>
                 <nav className="menu menu--social mb-4">
                     <ul>
-                        <li><a className="icon" href="https://www.linkedin.com/in/hyungmo-gu/"><i className="fab fa-linkedin-in"></i></a></li>
-                        <li><a className="icon" href="https://github.com/hyungmogu"><i className="fab fa-github"></i></a></li>
+                        <li><a className="icon" href="https://www.linkedin.com/in/hyungmo-gu/"><FontAwesomeIcon icon={["fab", "linkedin-in"]} /></a></li>
+                        <li><a className="icon" href="https://github.com/hyungmogu"><FontAwesomeIcon icon={["fab", "github"]} /></a></li>
                     </ul>
                     <div>Hyungmo Gu</div>
                     <div className="small">Thank you for visiting!</div>

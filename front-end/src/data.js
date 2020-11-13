e85b93c5f593e81b9f4193aadab56fc494ee32db
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-14 01:48:21 +0900

    feat: Add progress #124

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -2,7 +2,7 @@ export const data = {
     "works": [
         {
             "id": 1,
-            "title": "Photo App",
+            "title": "Photo Application",
             "date": "January 8th, 2020",
             "headerImage": "https://user-images.githubusercontent.com/72232928/99029105-f46c2080-25b4-11eb-88fe-a6471bf07721.png",
             "shortDescription": "Basic photo & photo gallery app built using React native",
@@ -20,6 +20,26 @@ export const data = {
                 "https://user-images.githubusercontent.com/72232928/99028879-54ae9280-25b4-11eb-992a-38b9da918b29.jpg",
                 "https://user-images.githubusercontent.com/72232928/99028882-56785600-25b4-11eb-9472-76ea22afe64e.jpg",
                 "https://user-images.githubusercontent.com/72232928/99028886-58421980-25b4-11eb-97e4-7a1bf65a433d.jpg"
+            ]
+        },
+        {
+            "id": 2,
+            "title": "Calculator",
+            "date": "January 5th, 2020",
+            "headerImage": "https://user-images.githubusercontent.com/72232928/99029105-f46c2080-25b4-11eb-88fe-a6471bf07721.png",
+            "shortDescription": "Calculator web app built using React.js",
+            "demoURL": "https://expo.io/@guhyungm7/basic-photo-app",
+            "sourceURL": "https://github.com/hyungmogu/basic-photo-app#",
+            "techUsed": [
+                {
+                    "name": "Frontend",
+                    "items": ["SCSS", "React Native"]
+                }
+            ],
+            "images": [
+                "https://user-images.githubusercontent.com/72232928/99097699-552e4400-261b-11eb-91aa-2106219c8885.png",
+                "https://user-images.githubusercontent.com/72232928/99097707-565f7100-261b-11eb-9824-08436888305c.png",
+                "https://user-images.githubusercontent.com/72232928/99097710-56f80780-261b-11eb-9be2-96674842a62f.png"
             ]
         }
     ]

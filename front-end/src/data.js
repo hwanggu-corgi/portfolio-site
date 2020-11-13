26792de41ae861c175a7f888b8816c727f490202
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-14 01:18:14 +0900

    feat: Add progress #122

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -29,6 +29,28 @@ export const data = {
                 "https://user-images.githubusercontent.com/72232928/99028882-56785600-25b4-11eb-9472-76ea22afe64e.jpg",
                 "https://user-images.githubusercontent.com/72232928/99028886-58421980-25b4-11eb-97e4-7a1bf65a433d.jpg"
             ]
+        },
+        {
+            "id": 2,
+            "title": "Photo App",
+            "date": "January 8th, 2020",
+            "headerImage": "https://user-images.githubusercontent.com/72232928/99029105-f46c2080-25b4-11eb-88fe-a6471bf07721.png",
+            "shortDescription": "Basic photo & photo gallery app built using React native",
+            "demoURL": "https://expo.io/@guhyungm7/basic-photo-app",
+            "sourceURL": "https://github.com/hyungmogu/basic-photo-app#",
+            "techUsed": [
+                {
+                    "name": "Frontend",
+                    "items": [" React Native"]
+                }
+            ],
+            "images": [
+                "https://user-images.githubusercontent.com/72232928/99028888-59734680-25b4-11eb-94d9-c5c8cde63c05.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028873-511b0b80-25b4-11eb-92c1-81ebbd520811.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028879-54ae9280-25b4-11eb-992a-38b9da918b29.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028882-56785600-25b4-11eb-9472-76ea22afe64e.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028886-58421980-25b4-11eb-97e4-7a1bf65a433d.jpg"
+            ]
         }
     ]
 };

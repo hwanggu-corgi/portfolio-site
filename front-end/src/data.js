43df2c347fcda75e47bf096a49600780d0334a05
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-13 13:35:23 +0900

    feat: Add progress #115

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -2,9 +2,9 @@ export const data = {
     "works": [
         {
             "id": 1,
-            "title": "Work #1",
+            "title": "Photo App",
             "date": "August 15th, 2020",
-            "headerImage": "http://placehold.it/200x200",
+            "headerImage": "https://user-images.githubusercontent.com/72232928/99029105-f46c2080-25b4-11eb-88fe-a6471bf07721.png",
             "shortDescription": "Portfolio short description goes here",
             "techUsed": [
                 {
@@ -19,6 +19,13 @@ export const data = {
                     "name": "Database",
                     "items": ["Database1"]
                 }
+            ],
+            "images": [
+                "https://user-images.githubusercontent.com/72232928/99028888-59734680-25b4-11eb-94d9-c5c8cde63c05.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028873-511b0b80-25b4-11eb-92c1-81ebbd520811.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028879-54ae9280-25b4-11eb-992a-38b9da918b29.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028882-56785600-25b4-11eb-9472-76ea22afe64e.jpg",
+                "https://user-images.githubusercontent.com/72232928/99028886-58421980-25b4-11eb-97e4-7a1bf65a433d.jpg"
             ]
         }
     ]

d232388f0157cf0696f9ed7b0455ee3f608865ef
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-20 00:13:23 +0900

    feat: Add progress #142

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -5,10 +5,13 @@ class HomeScreen extends Component {
     render() {
         return (
             <section className="content content-home">
-                <div class="background-img"></div>
-                <article class="article--home">
-                    <h1 class="title">Hello,</h1>
-                    <h1>Welcome to the portfolio site of Hyungmo Gu</h1>
+                <div className="background-img"></div>
+                <article className="article--home">
+                    <h1 className="title typing"><div>Hello,</div></h1>
+                    <h1>
+                        <div>Welcome to the</div>
+                        <div>portfolio site of</div>
+                        <div>Hyungmo Gu</div></h1>
                     <strong>A Calgary based software developer who loves to learn, create new app, and push codes to github with his love</strong>
                 </article>
             </section>

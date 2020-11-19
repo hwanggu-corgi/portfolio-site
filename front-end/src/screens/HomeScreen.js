768061a27acad0bcf8ff9f7e8c5c9e767976f2fb
hwanggu-corgi <hwanggumedia@gmail.com>
2020-11-20 02:51:13 +0900

    feat: Add progress #149

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
-import Typed from 'typed.js';
 
 class HomeScreen extends Component {
-
-    componentDidMount() {
-        var options = {
-        strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-        typeSpeed: 40
-        };
-
-        var typed = new Typed('.element', options);
-    }
 
     render() {
         return (

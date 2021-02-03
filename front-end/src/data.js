ff5016d483fb4709f702ebc572db2ad58a5a8841
Corgi <hwanggumedia@gmail.com>
2021-02-04 05:04:56 +0900

    feat: Add progress #198

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -36,7 +36,8 @@ export const data = {
                 "https://user-images.githubusercontent.com/6856382/106783115-442b2700-668e-11eb-9437-ddf64a11e361.png",
                 "https://user-images.githubusercontent.com/6856382/106783195-5c9b4180-668e-11eb-84c8-1547f0b7872a.png",
                 "https://user-images.githubusercontent.com/6856382/106783374-95d3b180-668e-11eb-9a6b-12292c6aba7d.png",
-                "https://user-images.githubusercontent.com/6856382/106795910-06ce9580-669e-11eb-8a9f-8f4376cf4fd9.png",
+                "https://user-images.githubusercontent.com/6856382/106802511-6761d080-66a6-11eb-8904-37808b7d8c3c.png",
+                "https://user-images.githubusercontent.com/6856382/106802426-4c8f5c00-66a6-11eb-955f-3f9e20dbe619.png",
                 "https://user-images.githubusercontent.com/72232928/99109994-07bad280-262d-11eb-8511-61ccc5f5e1f4.png",
                 "https://user-images.githubusercontent.com/72232928/99109996-09849600-262d-11eb-8f80-e0a889bc0c7e.png"
             ]

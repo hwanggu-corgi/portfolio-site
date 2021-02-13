b88c6a6947178680c09d415547b1494ec9c76d0c
Corgi <hwanggumedia@gmail.com>
2021-02-13 11:05:07 +0900

    feat: Add progress #229

---
+++ front-end/src/screens/lib.js
@@ -0,0 +1,25 @@
+export function Screen(props) {
+    if () {
+        return (
+            <section className="content content-home">
+                <div className="background-img"></div>
+                <article className="article--home">
+                    {props.children}
+                </article>
+            </section>
+        );
+    } else {
+        return (
+            <section className="content content-contacts">
+                <header class="header--secondary p-6">
+                    <h2>Contact</h2>
+                </header>
+                <div class="ml-4 mr-4">
+                    <article class="p-6">
+                        {props.children}
+                    </article>
+                </div>
+            </section>
+        );
+    }
+}

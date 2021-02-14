e65981864698e06dfe494b1dd675c28e4c0947f8
Corgi <hwanggumedia@gmail.com>
2021-02-14 11:22:27 +0900

    feat: Add progress #246

--- front-end/src/screens/lib.js
+++ front-end/src/screens/lib.js
@@ -1,8 +1,6 @@
 import { useLocation } from 'react-router-dom'
 import styled from 'styled-components';
-
-import HomeBackgroundImg from '../media/cover-page.jpg';
-import { headerHeight } from '../components/Navigation';
+import { headerWidthDesktop } from '../components/Navigation';
 
 export const H1 = styled.h1`
     margin-top: 0;
@@ -14,6 +12,17 @@ export const H3 = styled.h3`
 
 export function Screen(props) {
     const location = useLocation();
+
+    const Section = styled.section`
+        width: 100vw;
+        margin-top: 4.0rem;
+
+        @media screen and (min-width: 930px) {
+            width: calc(100vw - ${headerWidthDesktop});
+            margin-top: 0;
+        }
+    `;
+
     if (location.pathname === "/") {
         const Article = styled.article`
             margin: 5rem 2rem 0 2rem;
@@ -27,32 +36,13 @@ export function Screen(props) {
             }
         `;
 
-        // TEMP for now
-        const BackgroundImg = styled.div`
-            width: inherit;
-            padding-top: ${headerHeight};
-            height: calc(100vh - ${headerHeight});
-            position: absolute;
-            z-index: -1;
-            top: 0;
-            background-image: url(${HomeBackgroundImg});
-            background-repeat: no-repeat;
-            background-size: cover;
-
-            @media screen and (min-width: 930px) {
-                padding-top: 0;
-                height: 100vh;
-            }
-
-        `;
-
         return (
-            <section>
-                <BackgroundImg/>
+            <Section>
+                <div className="background-img"></div>
                 <Article>
                     {props.children}
                 </Article>
-            </section>
+            </Section>
         );
     } else if (location.pathname === "/works") {
         const Article = styled.article`
@@ -61,25 +51,25 @@ export function Screen(props) {
         `;
 
         return (
-            <section className="content">
+            <Section>
                 <header className="header--secondary p-6">
                     <h2>{props.title}</h2>
                 </header>
                 <Article className="p-6 ml-4 mr-4">
                     {props.children}
                 </Article>
-            </section>
+            </Section>
         );
     } else {
         return (
-            <section className="content">
+            <Section>
                 <header className="header--secondary p-6">
                     <h2>{props.title}</h2>
                 </header>
                 <article className="p-6 ml-4 mr-4">
                     {props.children}
                 </article>
-            </section>
+            </Section>
         );
     }
 }

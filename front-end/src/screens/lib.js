01133ed9796f094bb94337c1264c0cde13d76834
Corgi <hwanggumedia@gmail.com>
2021-02-14 13:45:43 +0900

    feat: Add progress #247

--- front-end/src/screens/lib.js
+++ front-end/src/screens/lib.js
@@ -13,13 +13,14 @@ export const H3 = styled.h3`
 export function Screen(props) {
     const location = useLocation();
 
-    const Section = styled.section`
+    const ContentSection = styled.section`
         width: 100vw;
         margin-top: 4.0rem;
 
         @media screen and (min-width: 930px) {
             width: calc(100vw - ${headerWidthDesktop});
             margin-top: 0;
+            overflow-y: scroll;
         }
     `;
 
@@ -37,12 +38,12 @@ export function Screen(props) {
         `;
 
         return (
-            <Section>
+            <ContentSection>
                 <div className="background-img"></div>
                 <Article>
                     {props.children}
                 </Article>
-            </Section>
+            </ContentSection>
         );
     } else if (location.pathname === "/works") {
         const Article = styled.article`
@@ -51,25 +52,25 @@ export function Screen(props) {
         `;
 
         return (
-            <Section>
+            <ContentSection>
                 <header className="header--secondary p-6">
                     <h2>{props.title}</h2>
                 </header>
                 <Article className="p-6 ml-4 mr-4">
                     {props.children}
                 </Article>
-            </Section>
+            </ContentSection>
         );
     } else {
         return (
-            <Section>
+            <ContentSection>
                 <header className="header--secondary p-6">
                     <h2>{props.title}</h2>
                 </header>
                 <article className="p-6 ml-4 mr-4">
                     {props.children}
                 </article>
-            </Section>
+            </ContentSection>
         );
     }
 }

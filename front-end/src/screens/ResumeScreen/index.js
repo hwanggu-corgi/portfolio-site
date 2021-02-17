15edf25ccfeea5c2c1be9283b55337f93ad13d3e
Corgi <hwanggumedia@gmail.com>
2021-02-18 05:10:21 +0900

    feat: Add progress #259

--- front-end/src/screens/ResumeScreen/index.js
+++ front-end/src/screens/ResumeScreen/index.js
@@ -7,15 +7,27 @@ import { Screen } from '../lib';
 
 
 class ResumeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.resumeRef = React.createRef();
+    }
+
+    // ref: https://stackoverflow.com/questions/12997123/print-specific-part-of-webpage
+    print = () => {
+        console.log(this.resumeRef);
+        // var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+        // WinPrint.document.write(this.props.children);
+        // WinPrint.focus();
+        // WinPrint.print();
+    }
+
     render() {
         const data = this.props.appContext.data;
         const pdfURL = this.props.appContext.data.resume.pdfURL;
         return (
             <Screen title={"Resume"}>
-                <Resume data={data}/>
-                <PrintButton>
-                    <Resume data={data}/>
-                </PrintButton>
+                <Resume data={data} ref={this.resumeRef}/>
+                <PrintButton onClick={this.print}/>
             </Screen>
         );
     }

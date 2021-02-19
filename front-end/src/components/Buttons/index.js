3ba643d716c183f69253b29898147bacd6eac6ed
Corgi <hwanggumedia@gmail.com>
2021-02-20 03:19:53 +0900

    feat: Add progress #266

--- front-end/src/components/Buttons/index.js
+++ front-end/src/components/Buttons/index.js
@@ -6,12 +6,32 @@ import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
 export function PrintButton(props) {
   const Button = styled.button`
+    border: none;
+    background-color: white;
+    border-radius: 50%;
+    box-shadow: 3px 5px 8px grey;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    font-size: 1.5em;
+    text-decoration: none;
+    color: inherit;
+    padding: 1em;
+    margin: 1.25em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-
+    div {
+      width: 1em;
+      height: 1em;
+    }
   `;
   return (
     <Button onClick={props.onClick}>
-        <FontAwesomeIcon icon={faFilePdf} />
+        <div>
+          <FontAwesomeIcon icon={faFilePdf} />
+        </div>
     </Button>
   );
 }
